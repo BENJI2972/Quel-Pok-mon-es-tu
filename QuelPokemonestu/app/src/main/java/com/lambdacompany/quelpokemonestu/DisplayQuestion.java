10eb5dbbc13a55ca507c2f330e0f28aeadb0c4bb
lstaff <prakkmak@gmail.com>
2016-08-06 16:21:24 +0200

    Update

--- QuelPokemonestu/app/src/main/java/com/lambdacompany/quelpokemonestu/DisplayQuestion.java
+++ QuelPokemonestu/app/src/main/java/com/lambdacompany/quelpokemonestu/DisplayQuestion.java
@@ -3,12 +3,14 @@ package com.lambdacompany.quelpokemonestu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class DisplayQuestion extends AppCompatActivity {
+    public Question[] Questions = new Question[30];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_display_question);
+    }
+    void displayQuestion(){
 
     }
 }
---
+++ QuelPokemonestu/app/src/main/java/com/lambdacompany/quelpokemonestu/Question.java
@@ -0,0 +1,14 @@
+package com.lambdacompany.quelpokemonestu;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by lstaff on 06/08/2016.
+ */
+
+public class Question {
+    public String text;
+    public String[] reponse = new String[4];
+}
